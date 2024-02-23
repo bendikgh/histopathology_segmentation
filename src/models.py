43b259c48596a4974ce2df2d2fa287f012af33b5
Jarl Sæther <jarl_sondre@hotmail.com>
2024-02-23 11:50:36 +0100

    add model class

---
+++ src/models.py
@@ -0,0 +1,66 @@
+from torch import nn
+from deeplabv3.network.utils import IntermediateLayerGetter
+from deeplabv3.network.backbone import resnet
+
+# from deeplabv3.network.modeling import _segm_resnet
+from deeplabv3.network._deeplab import (
+    DeepLabHeadV3Plus,
+    DeepLabV3,
+)
+
+
+class DeepLabV3plusModel(nn.Module):
+
+    def __init__(
+        self,
+        backbone_name: str,
+        num_classes: int,
+        num_channels: int,
+        pretrained: bool,
+        dropout_rate: float,
+    ):
+        if num_channels < 3:
+            raise ValueError("Number of input channels must be at least 3")
+
+        super().__init__()
+        # Assuming an output stride of 8
+        aspp_dilate = [12, 24, 36]
+
+        if backbone_name == "resnet34":
+            replace_stride_with_dilation = [False, False, False]
+            low_level_planes = 64
+            inplanes = 512
+        else:
+            replace_stride_with_dilation = [False, True, True]
+            low_level_planes = 256
+            inplanes = 2048
+
+        backbone = resnet.__dict__[backbone_name](
+            pretrained=pretrained,
+            replace_stride_with_dilation=replace_stride_with_dilation,
+            dropout_rate=dropout_rate,
+        )
+
+        # Updating the first layer to accept the correct number of channels
+        input_layer = backbone.conv1
+        new_conv1 = nn.Conv2d(
+            num_channels,
+            input_layer.out_channels,
+            kernel_size=input_layer.kernel_size,
+            stride=input_layer.stride,
+            padding=input_layer.padding,
+        )
+
+        # Copying the three first channels from the original weights
+        new_conv1.weight.data[:, :3] = input_layer.weight.data
+        backbone.conv1 = new_conv1
+
+        return_layers = {"layer4": "out", "layer1": "low_level"}
+        classifier = DeepLabHeadV3Plus(
+            inplanes, low_level_planes, num_classes, aspp_dilate
+        )
+        backbone = IntermediateLayerGetter(backbone, return_layers=return_layers)
+        self.model = DeepLabV3(backbone, classifier)
+
+    def forward(self, x):
+        return self.model(x)
