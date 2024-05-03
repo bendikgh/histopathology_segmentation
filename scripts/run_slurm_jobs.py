6e1113e9647196f29595fc32916796448c698aa2
Jarl Sondre Sæther <jssaethe@stud.ntnu.no>
2024-05-03 17:42:09 +0200

    update run slurm script

--- scripts/run_slurm_jobs.py
+++ scripts/run_slurm_jobs.py
@@ -2,6 +2,75 @@ import datetime
 import os
 import subprocess
 
+from typing import List
+
+
+def generate_python_arg_list(
+    python_file: str,
+    model_architecture: str,
+    epochs,
+    batch_size,
+    checkpoint_interval,
+    backbone,
+    dropout,
+    learning_rate,
+    learning_rate_end,
+    pretrained,
+    warmup_epochs,
+    do_save,
+    do_eval,
+    break_early,
+    normalization,
+    resize,
+    pretrained_dataset,
+    leak_labels,
+    loss_function,
+    id_,
+) -> List:
+    # Return a list of command components instead of a single string
+    return [
+        "python",
+        python_file,
+        "--model-architecture",
+        model_architecture,
+        "--epochs",
+        str(epochs),
+        "--batch-size",
+        str(batch_size),
+        "--checkpoint-interval",
+        str(checkpoint_interval),
+        "--backbone",
+        backbone,
+        "--dropout",
+        str(dropout),
+        "--learning-rate",
+        str(learning_rate),
+        "--learning-rate-end",
+        str(learning_rate_end),
+        "--pretrained",
+        str(pretrained),
+        "--warmup-epochs",
+        str(warmup_epochs),
+        "--do-save",
+        str(do_save),
+        "--do-eval",
+        str(do_eval),
+        "--break-early",
+        str(break_early),
+        "--normalization",
+        normalization,
+        "--resize",
+        str(resize),
+        "--pretrained-dataset",
+        pretrained_dataset,
+        "--leak-labels",
+        str(leak_labels),
+        "--loss-function",
+        loss_function,
+        "--id",
+        str(id_),
+    ]
+
 
 def generate_slurm_script(
     job_name: str,
@@ -30,6 +99,29 @@ def generate_slurm_script(
 ):
     current_time = datetime.datetime.now().strftime("%Y-%m-%d")
     output_str = f"{current_time}/{job_name}_normalize_{normalization.replace('+', '_').replace(' ', '')}_id-{id_}"
+    python_arg_list = generate_python_arg_list(
+        python_file=python_file,
+        model_architecture=model_architecture,
+        epochs=epochs,
+        batch_size=batch_size,
+        checkpoint_interval=checkpoint_interval,
+        backbone=backbone,
+        dropout=dropout,
+        learning_rate=learning_rate,
+        learning_rate_end=learning_rate_end,
+        pretrained=pretrained,
+        warmup_epochs=warmup_epochs,
+        do_save=do_save,
+        do_eval=do_eval,
+        break_early=break_early,
+        normalization=normalization,
+        resize=resize,
+        pretrained_dataset=pretrained_dataset,
+        leak_labels=leak_labels,
+        loss_function=loss_function,
+        id_=id_,
+    )
+    python_call = " ".join(python_arg_list)
     return f"""#!/bin/sh
 
 #SBATCH --job-name={job_name}
@@ -56,26 +148,7 @@ module purge
 module load Anaconda3/2022.10
 conda activate specialization_project
 
-python {python_file} \\
-  --model-architecture "{model_architecture}" \\
-  --epochs {epochs} \\
-  --batch-size {batch_size} \\
-  --checkpoint-interval {checkpoint_interval} \\
-  --backbone {backbone} \\
-  --dropout {dropout} \\
-  --learning-rate {learning_rate} \\
-  --learning-rate-end {learning_rate_end} \\
-  --pretrained {pretrained} \\
-  --warmup-epochs {warmup_epochs} \\
-  --do-save {do_save} \\
-  --do-eval {do_eval} \\
-  --break-early {break_early} \\
-  --normalization "{normalization}" \\
-  --resize {resize} \\
-  --pretrained-dataset "{pretrained_dataset}" \\
-  --leak-labels {leak_labels} \\
-  --loss-function {loss_function} \\
-  --id {id_}"""
+{python_call}"""
 
 
 def main():
@@ -84,7 +157,7 @@ def main():
     #SBATCH --constraint="gpu32g|gpu40g|gpu80g"
     """
 
-    run_script = False
+    run_slurm = False
 
     # General parameters
     job_name = "exp5-tc-b3-512"
@@ -95,7 +168,7 @@ def main():
     # Script-specific parameters
     # "segformer_cell_only", "segformer_tissue_branch", "segformer_cell_branch", "segformer_joint_pred2input", "segformer_sum_sharing", "deeplab_cell_only", "deeplab_tissue_cell", "vit_unet"
     model_architecture = "segformer_joint_pred2input"
-    epochs = 5
+    epochs = 100
     batch_size = 2
     checkpoint_interval = 10
     backbone = "b3"
@@ -103,7 +176,7 @@ def main():
     learning_rate = 6e-5
     learning_rate_end = 2e-5
     pretrained = 1
-    warmup_epochs = 0
+    warmup_epochs = 10
     do_save = 1
     do_eval = 1
     break_early = 0
@@ -146,7 +219,7 @@ def main():
         )
         script_filename = f"scripts/slurm_script_id{id_}.sh"
 
-        if run_script:
+        if run_slurm:
             with open(script_filename, "w") as script_file:
                 script_file.write(script_contents)
 
@@ -156,7 +229,30 @@ def main():
             os.remove(script_filename)
             print(f"Deleted: {script_filename}")
         else:
-            print(script_contents)
+            python_arg_list = generate_python_arg_list(
+                python_file=python_file,
+                model_architecture=model_architecture,
+                epochs=epochs,
+                batch_size=batch_size,
+                checkpoint_interval=checkpoint_interval,
+                backbone=backbone,
+                dropout=dropout,
+                learning_rate=learning_rate,
+                learning_rate_end=learning_rate_end,
+                pretrained=pretrained,
+                warmup_epochs=warmup_epochs,
+                do_save=do_save,
+                do_eval=do_eval,
+                break_early=break_early,
+                normalization=normalization,
+                resize=resize,
+                pretrained_dataset=pretrained_dataset,
+                leak_labels=leak_labels,
+                loss_function=loss_function,
+                id_=id_,
+            )
+            print(f"Running:\n {' '.join(python_arg_list)}")
+            subprocess.run(python_arg_list, capture_output=False)
 
 
 if __name__ == "__main__":
